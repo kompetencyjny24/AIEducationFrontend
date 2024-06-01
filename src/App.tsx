c12dbf870d0c5db582b5203eb7e34bcc288ad268
jaqubm <jaqobgm@gmail.com>
2024-06-01 15:15:57 +0200

    Added react-router-dom and configured it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+
+        <Route
+        path="/"
+        element={
+          <div className="h-screen w-screen flex justify-center items-center">
+            <h1 className="text-3xl font-bold underline">
+              Hello world!
+            </h1>
+          </div>
+        } />
+
+        <Route
+        path="/test"
+        element={
+          <div className="h-screen w-screen flex justify-center items-center">
+            <h1 className="text-3xl font-bold underline">
+              Test
+            </h1>
+          </div>
+        } />
+
+      </Routes>
+    </Router>
   );
 }
 
